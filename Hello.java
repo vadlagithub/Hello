8c1c6a3e79ce9fcffd3862170c60fa180d5fb83c
VadlaS <prashanthboggu2@gmail.com>
2018-04-13 23:14:25 +0530

    Hello World prog

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{	
+
+    public static void main(String[] args)
+	{
+		int i;
+         for(i=10;i<20;i++){
+              System.out.println("Vadla thop!!@ "+i);
+         }
+    }
+}
